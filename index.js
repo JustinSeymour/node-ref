be3449901a6555dcb7789f936bd295f469345bb8
Justin Seymour <justin@3wmholdings.com>
2018-08-14 17:42:32 +0200

    Section 4 - post

--- index.js
+++ index.js
@@ -28,50 +28,70 @@ httpsServer.listen(config.httpsPort, function() {
     console.log('The server is listening on port '+config.httpsPort);
 });
 
-var unifiedServer = function (req, res) {
-    var parsedUrl = url.parse(req.url, true);
-
-    var path = parsedUrl.pathname;
-    var trimmmedPath = path.replace(/^\/+|\/+$/g, '');
-
-    var queryStringObject = parsedUrl.query;
-
-    var method = req.method.toLowerCase();
-    var headers = req.headers;
-
-    var decoder = new StringDecoder('utf-8');
-    var buffer = '';
-    req.on('data', function (data) {
-        buffer += decoder.write(data);
-    });
-    req.on('end', function () {
-        buffer += decoder.end();
-
-        var chosenHandler = typeof(router[trimmmedPath]) !== 'undefined' ? router[trimmmedPath] : handlers.notFound;
-
-        var data = {
-            'trimmmedPath ': trimmmedPath,
-            'queryStringObject': queryStringObject,
-            'method': method,
-            'headers': headers,
-            'payload': helpers.parseJsonToObject(buffer)
-        };
-
-        chosenHandler(data, function (statusCode, payload) {
+var unifiedServer = function(req,res){
+    
+      // Parse the url
+      var parsedUrl = url.parse(req.url, true);
+    
+      // Get the path
+      var path = parsedUrl.pathname;
+      var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    
+      // Get the query string as an object
+      var queryStringObject = parsedUrl.query;
+    
+      // Get the HTTP method
+      var method = req.method.toLowerCase();
+    
+      //Get the headers as an object
+      var headers = req.headers;
+    
+      // Get the payload,if any
+      var decoder = new StringDecoder('utf-8');
+      var buffer = '';
+      req.on('data', function(data) {
+          buffer += decoder.write(data);
+      });
+      req.on('end', function() {
+          buffer += decoder.end();
+    
+          // Check the router for a matching path for a handler. If one is not found, use the notFound handler instead.
+          var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+    
+          // Construct the data object to send to the handler
+          var data = {
+            'trimmedPath' : trimmedPath,
+            'queryStringObject' : queryStringObject,
+            'method' : method,
+            'headers' : headers,
+            'payload' : helpers.parseJsonToObject(buffer)
+          };
+    
+          // Route the request to the handler specified in the router
+          chosenHandler(data,function(statusCode,payload){
+    
+            // Use the status code returned from the handler, or set the default status code to 200
             statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-            payload = typeof(payload) == 'object' ? payload : {};
+    
+            // Use the payload returned from the handler, or set the default payload to an empty object
+            payload = typeof(payload) == 'object'? payload : {};
+    
+            // Convert the payload to a string
             var payloadString = JSON.stringify(payload);
+    
+            // Return the response
             res.setHeader('Content-Type', 'application/json');
             res.writeHead(statusCode);
             res.end(payloadString);
-            console.log('Returning this response: ', statusCode, payload);
-        });
-    });
-};
-
-
+            console.log(trimmedPath,statusCode);
+          });
+    
+      });
+    };
 
 var router = {
     'ping': handlers.ping,
-    'users': handlers.users
+    'users': handlers.users,
+    'tokens' : handlers.tokens,
+    'checks' : handlers.checks
 };
